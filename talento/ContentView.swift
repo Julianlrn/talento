4dc39049095fa12b1fb640aa6e20030ef4d59bb9
Gökay Inan <inangokay@hotmail.com>
2019-12-18 16:17:04 +0100

    Neue TabNav

--- talento/ContentView.swift
+++ talento/ContentView.swift
@@ -11,52 +11,105 @@ import FBSDKLoginKit
 import Firebase
 import FirebaseUI
 
-
 struct ContentView: View {
-    
-    @EnvironmentObject var loginStatus:LoginCheck
-    
+    @State var index = 0
     var body: some View {
         
-        /*if loginStatus.loading == true {
+        /*
+        @EnvironmentObject var loginStatus:LoginCheck
+        if loginStatus.loading == true {
             return AnyView(Text("Loading"))
         }
         else if loginStatus.loggedIn == false {
             return AnyView(login().frame(width: 100, height: 50))
-        }*/
-            return AnyView(
-                            TabView{
-                                FeedView().tabItem {
-                                        Image(systemName: "house")
-                                        Text("Feed")
-                                    }.tag(0)
-                                ChallengeView().tabItem {
-                                        Image(systemName: "bolt")
-                                        Text("Challenges")
-                                    }.tag(1)
-                                ActivityView().tabItem {
-                                        Image(systemName: "suit.heart")
-                                        Text("Activity")
-                                    }.tag(2)
-                                ProfileView().tabItem {
-                                        Image(systemName: "person")
-                                        Text("Profile")
-                                    }.tag(3)
-                                }
-                            )
-    }
-   
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environmentObject(LoginCheck())
+        }
+        */
         
+        VStack {
+            
+            ZStack {
+                
+                if self.index == 0 {
+                    Color.red.edgesIgnoringSafeArea(.top)
+                }
+                else if self.index == 1 {
+                    Color.blue.edgesIgnoringSafeArea(.top)
+                }
+                else if self.index == 2 {
+                    Color.green.edgesIgnoringSafeArea(.top)
+                }
+                else if self.index == 3 {
+                    Color.yellow.edgesIgnoringSafeArea(.top)
+                }
+                else {
+                    Color.white.edgesIgnoringSafeArea(.top)
+                }
+            }
+            
+            TabBar(index : $index)
+            
+        }.animation(.spring())
     }
 }
 
+struct ContentView_Preview: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
 
+struct TabBar : View {
     
+    @Binding var index : Int
+    
+    var body: some View {
+        
+        HStack(spacing: 15) {
+            
+            HStack {
+                
+                Image(systemName: "house").resizable().frame(width: 26, height: 24)
+                Text(self.index == 0 ? "Feed" : "").fontWeight(.light).font(.system(size: 14))
+            }.padding(15)
+            .clipShape(Capsule())
+            .onTapGesture {
+                self.index = 0
+            }
+            
+            HStack {
+                
+                Image(systemName: "bolt")
+                    .resizable().frame(width: 15, height: 24)
+                Text(self.index == 1 ? "Challenges" : "").fontWeight(.light).font(.system(size: 14))
+            }.padding(15)
+            .clipShape(Capsule())
+            .onTapGesture {
+                self.index = 1
+            }
+            
+            HStack {
+                
+                Image(systemName: "suit.heart").resizable().frame(width: 25, height: 24)
+                Text(self.index == 2 ? "Activity" : "").fontWeight(.light).font(.system(size: 14))
+            }.padding(15)
+            .clipShape(Capsule())
+            .onTapGesture {
+                self.index = 2
+            }
+            
+            HStack {
+                
+                Image(systemName: "person").resizable().frame(width: 24, height: 24)
+                Text(self.index == 3 ? "Profile" : "").fontWeight(.light).font(.system(size: 14))
+            }.padding(15)
+            .clipShape(Capsule())
+            .onTapGesture {
+                self.index = 3
+            }
 
-
-
+        }.padding(.top, 8)
+        .frame(width: UIScreen.main.bounds.width)
+        .background(Color.white)
+        .animation(.default)
+    }
+}
