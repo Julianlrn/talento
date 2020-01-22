22bf7bd44a585933886c2e50a568a4f71fc1cbef
Julian Lorenz <JulianBLorenz@gmail.com>
2020-01-22 23:03:03 +0100

    ui

--- talento/View/TabBarViews/ChallengesView.swift
+++ talento/View/TabBarViews/ChallengesView.swift
@@ -19,63 +19,61 @@ struct ChallengeView: View {
     //var challenges: [Challenge] = getChallengeData()
     
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false) {
-            VStack(alignment: .leading) {
-                Text("Challenges")
-                    .font(.system(size: 32, weight: .bold))
-                    .padding(.top, 16)
-                    .padding(.leading, 16)
-                HStack(spacing: 16) {
-                    HStack {
-                        Text("Public")
-                            .foregroundColor(self.tabIndex == 0 ? Color.init(red:1.00, green:1.00, blue:1.00) : Color.init(red:0.00, green:0.00, blue:0.00, opacity: 0.32))
-                    }
-                    .padding(16)
-                    .background(self.tabIndex == 0 ? Color.init(red:0.96, green:0.11, blue:0.34) : Color.init(red:0.00, green:0.00, blue:0.00, opacity: 0.06))
-                    .cornerRadius(16)
-                    .onTapGesture {
-                        self.tabIndex = 0
-                    }
-                    
-                    HStack {
-                        Text("Local")
-                        .foregroundColor(self.tabIndex == 1 ? Color.init(red:1.00, green:1.00, blue:1.00) : Color.init(red:0.00, green:0.00, blue:0.00, opacity: 0.32))
-                    }
-                    .padding(16)
-                    .background(self.tabIndex == 1 ? Color.init(red:0.96, green:0.11, blue:0.34) : Color.init(red:0.00, green:0.00, blue:0.00, opacity: 0.06))
-                    .cornerRadius(16)
-                    .onTapGesture {
-                        self.tabIndex = 1
-                    }
-                }
-                .padding(.top, 16)
-                .padding(.bottom, 16)
-                .padding(.leading, 16)
-                
-                if self.tabIndex == 0 {
+            NavigationView {
+                ScrollView(.vertical, showsIndicators: false) {
                     VStack(alignment: .leading) {
-                        Text("Hot")
-                            .font(.title)
-                            .padding(.leading, 16)
-                        HotChallenges()
-                    }
-                    
-                    VStack(alignment: .leading) {
-                        Text("New")
-                            .font(.title)
-                            .padding(.leading, 16)
-                        NewChallenges()
-                    }
-                    .offset(y: -80)
-                }
-                else if self.tabIndex == 1 {
-                    Text("Munich, Germany")
-                        .font(.title)
+                        HStack(spacing: 16) {
+                            HStack {
+                                Text("Public")
+                                    .foregroundColor(self.tabIndex == 0 ? Color.init(red:1.00, green:1.00, blue:1.00) : Color.init(red:0.00, green:0.00, blue:0.00, opacity: 0.32))
+                            }
+                            .padding(16)
+                            .background(self.tabIndex == 0 ? Color.init(red:0.96, green:0.11, blue:0.34) : Color.init(red:0.00, green:0.00, blue:0.00, opacity: 0.06))
+                            .cornerRadius(16)
+                            .onTapGesture {
+                                self.tabIndex = 0
+                            }
+                            
+                            HStack {
+                                Text("Local")
+                                .foregroundColor(self.tabIndex == 1 ? Color.init(red:1.00, green:1.00, blue:1.00) : Color.init(red:0.00, green:0.00, blue:0.00, opacity: 0.32))
+                            }
+                            .padding(16)
+                            .background(self.tabIndex == 1 ? Color.init(red:0.96, green:0.11, blue:0.34) : Color.init(red:0.00, green:0.00, blue:0.00, opacity: 0.06))
+                            .cornerRadius(16)
+                            .onTapGesture {
+                                self.tabIndex = 1
+                            }
+                        }
+                        .padding(.top, 16)
+                        .padding(.bottom, 16)
                         .padding(.leading, 16)
-                    LocalChallenges()
-                }
-            }
-            
+                        
+                        if self.tabIndex == 0 {
+                            VStack(alignment: .leading) {
+                                Text("Hot")
+                                    .font(.title)
+                                    .padding(.leading, 16)
+                                HotChallenges()
+                            }
+                            
+                            VStack(alignment: .leading) {
+                                Text("New")
+                                    .font(.title)
+                                    .padding(.leading, 16)
+                                NewChallenges()
+                            }
+                            .offset(y: -80)
+                        }
+                        else if self.tabIndex == 1 {
+                            Text("Munich, Germany")
+                                .font(.title)
+                                .padding(.leading, 16)
+                            LocalChallenges()
+                        }
+                    }
+                    
+                }.navigationBarTitle(Text("Challenges"))
         }
     }
 }
@@ -88,10 +86,13 @@ struct HotChallenges: View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack {
                 ForEach(challenges) { item in
-                    ChallengeCardView(challenge: item, imageLoader: ImageLoader(urlString: item.image))
-                        .frame(width: 311)
-                        .padding(.leading, 16)
-                        .padding(.vertical, 48)
+                    NavigationLink(destination: ChallengeDetailView(imageLoader: ImageLoader(urlString: item.image), challenge: item)) {
+                       ChallengeCardView(challenge: item, imageLoader: ImageLoader(urlString: item.image))
+                       .frame(width: 311)
+                       .padding(.leading, 16)
+                       .padding(.vertical, 48)
+                    }
+                    .buttonStyle(PlainButtonStyle())
                 }
             }
         }
@@ -107,10 +108,13 @@ struct NewChallenges: View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack {
                 ForEach(challenges) { item in
-                    ChallengeCardView(challenge: item, imageLoader: ImageLoader(urlString: item.image))
-                        .frame(width: 311)
-                        .padding(.leading, 16)
-                        .padding(.vertical, 48)
+                    NavigationLink(destination: ChallengeDetailView(imageLoader: ImageLoader(urlString: item.image), challenge: item)) {
+                       ChallengeCardView(challenge: item, imageLoader: ImageLoader(urlString: item.image))
+                       .frame(width: 311)
+                       .padding(.leading, 16)
+                       .padding(.vertical, 48)
+                    }
+                    .buttonStyle(PlainButtonStyle())
                 }
             }
         }
@@ -140,7 +144,10 @@ struct LocalChallenges: View {
             .padding(.bottom, 8)*/
          
             ForEach(challenges) { item in
-                ChallengeCardView(challenge: item, imageLoader: ImageLoader(urlString: item.image))
+                 NavigationLink(destination: ChallengeDetailView(imageLoader: ImageLoader(urlString: item.image), challenge: item)) {
+                                      ChallengeCardView(challenge: item, imageLoader: ImageLoader(urlString: item.image))
+                                   }
+                                   .buttonStyle(PlainButtonStyle())
             }
         }
         .padding(.horizontal, 16)
