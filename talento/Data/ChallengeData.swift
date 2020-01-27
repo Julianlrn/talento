7954d6315521e6347c152436f862aa2870ae6cc1
Gökay Inan <inangokay@hotmail.com>
2020-01-27 20:31:47 +0100

    Short Fix

--- talento/Data/ChallengeData.swift
+++ talento/Data/ChallengeData.swift
@@ -21,18 +21,18 @@ func getChallengeData() -> [Challenge]{
                 if (uids.contains(i.document.documentID)){
                     return}
                 let data: Challenge =
-             Challenge(
-                // id: i.document.documentID,
-                image: i.document.get("image") as! String,
-                // image: "mountain",
-                participants: 13241,
-                fbId: i.document.documentID,
-                title: i.document.get("title") as! String,
-                isPublic: i.document.get("isPublic") as? Bool,
-                timestamp: 60000,
-                instructions: i.document.get("instructions") as! String,
-                entry: [entry(id: "f69eZbgp2ALX4aSDSOXo", author: "michael0301", image: "sushi", likes: 2, swipe: 0, degree: 0)]
-             )
+                Challenge(
+                    // id: i.document.documentID,
+                    image: i.document.get("image") as! String,
+                    // image: "mountain",
+                    participants: 13241,
+                    fbId: i.document.documentID,
+                    title: i.document.get("title") as! String,
+                    isPublic: i.document.get("isPublic") as? Bool,
+                    timestamp: 60000,
+                    instructions: i.document.get("instructions") as! String,
+                    entry: [entry(id: "f69eZbgp2ALX4aSDSOXo", author: "michael0301", image: "sushi", likes: 2, swipe: 0, degree: 0)]
+                )
                 print("append")
                 print(data)
                 challengesData.append(data)
